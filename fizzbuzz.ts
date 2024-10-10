ccee0dd47b27bb0d52c8457b916aac52e02e1692
lhourquin <lhourquin@gmail.com>
2024-10-10 20:42:20 +0200

    improvement

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,13 +1,11 @@
 "use strict";
-/*
 //first solution
-function fizzBuzz(n: number): string[] {
+function fizzBuzz1(n: number): string[] {
     let number: number = 1;
     const arr: string[] = [];
     while (number != n + 1) {
         const divide_by_3: boolean = number % 3 == 0;
         const divide_by_5: boolean = number % 5 == 0;
-        console.log(number % 3 == 0);
         let str: string = String(number);
         if (divide_by_3) {
             str = "Fizz";
@@ -25,46 +23,49 @@ function fizzBuzz(n: number): string[] {
         number++;
     }
     return arr;
-};
-*/
+}
 //second solution with recursion
-function fizzBuzz(n: number): string[] {
-    //let number: number = 1;
+function fizzBuzz2(n: number): string[] {
     let arr: string[] = [];
-    let str: string = String(n);
     const newArr: string[] = [];
     if (n == 1) {
-        arr.push(str);
+        arr.push("1");
         return arr;
     }
 
     //while is not fizz or buzz or fizz buzz, push number, and call recursively function 
     if (n % 3 !== 0 && n % 5 !== 0) {
-        newArr.push(str);
-        const result = fizzBuzz(n - 1);
+        newArr.push(String(n));
+        const result = fizzBuzz2(n - 1);
         arr = result.concat(newArr);
-    }
-
-    if (n % 3 == 0 && n % 5 == 0) {
+    } else if (n % 3 == 0 && n % 5 == 0) {
         newArr.push("FizzBuzz");
-        const result = fizzBuzz(n - 1);
+        const result = fizzBuzz2(n - 1);
         arr = result.concat(newArr);
-    }
-
-    if (n % 3 == 0 && n % 5 != 0) {
+    } else if (n % 3 == 0) {
         newArr.push("Fizz");
-        const result = fizzBuzz(n - 1);
+        const result = fizzBuzz2(n - 1);
         arr = result.concat(newArr);
-    }
-
-    if (n % 5 == 0 && n % 3 != 0) {
+    } else {
         newArr.push("Buzz");
-        const result = fizzBuzz(n - 1);
+        const result = fizzBuzz2(n - 1);
         arr = result.concat(newArr);
     }
 
     return arr;
 }
 
-console.table(fizzBuzz(15));
+function fizzBuzz3(n: number): string[] {
+    const arr: string[] = [];
+    for (let i = 1; i <= n; i++) {
+        const divide_by_3: boolean = i % 3 == 0;
+        const divide_by_5: boolean = i % 5 == 0;
+        if (divide_by_5 && divide_by_3) arr.push("FizzBuzz")
+        else if (divide_by_3) arr.push("Fizz")
+        else if (divide_by_5) arr.push("Buzz")
+        else arr.push(String(i))
+    }
+    return arr;
+}
+console.table(fizzBuzz3(15));
 
